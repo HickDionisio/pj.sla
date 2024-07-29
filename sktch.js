6a2f415678db5f5d1c35aa9ca7e8fbe37ba23346
HickDionisio <00001102305698sp@al.educacao.sp.gov.br>
2024-07-29 14:57:57 -0300

    Coiso

---
+++ sktch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(600, 600);
+    background(10);
+  }
+  
+  function draw() {
+  
+    fill(98);
+    stroke(2000);
+  
+    // console. log (mouseIsPressed) ;
+  
+  if (mouseIsPressed){
+   rect(mouseX, mouseY,20,15)
+    }
+  }
+  
